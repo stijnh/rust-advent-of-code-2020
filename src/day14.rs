cdda0fafcaebe6c6542575be5068047bc1a137ae
stijn <git@stijnh.nl>
2020-12-21 21:13:15 +0100

    Finished day 14.

--- src/day14.rs
+++ src/day14.rs
@@ -3,7 +3,8 @@ use recap::Recap;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Debug)]
+use Instr::*;
+#[derive(Debug, Clone, Copy)]
 enum Instr {
     Mask(u64, u64),
     Assign(u64, u64),
@@ -26,15 +27,15 @@ fn parse_input(lines: &[String]) -> Result<Vec<Instr>> {
     let mut output = vec![];
     for line in lines {
         if let Ok(Mask { mask }) = line.parse() {
+            // X,0 -> 0, 1 -> 1
             let value = u64::from_str_radix(&mask.replace('X', "0"), 2).unwrap();
-            let mask = u64::from_str_radix(
-                &mask.replace(&['0', '1'] as &[char], "1").replace('X', "0"),
-                2,
-            )
-            .unwrap();
-            output.push(Instr::Mask(mask, value));
+
+            // 0,1 -> 0, X -> 1
+            let mask = u64::from_str_radix(&mask.replace('1', "0").replace('X', "1"), 2).unwrap();
+
+            output.push(Mask(mask, value));
         } else if let Ok(Assign { addr, value }) = line.parse() {
-            output.push(Instr::Assign(addr, value));
+            output.push(Assign(addr, value));
         } else {
             bail!("failed to parse line: {}", line);
         }
@@ -43,21 +44,55 @@ fn parse_input(lines: &[String]) -> Result<Vec<Instr>> {
     Ok(output)
 }
 
-fn execute(instrs: &[Instr]) -> HashMap<u64, u64> {
-    use Instr::*;
+fn execute_v1(instrs: &[Instr]) -> HashMap<u64, u64> {
     let mut mem = HashMap::new();
-    let mut mask_flags = 0;
-    let mut mask_values = 0;
+    let (mut mask, mut bits) = (0, 0);
 
-    for instr in instrs {
+    for &instr in instrs {
         match instr {
-            &Mask(m, v) => {
-                mask_flags = m;
-                mask_values = v & mask_flags;
+            Mask(m, v) => {
+                mask = m;
+                bits = v;
             }
-            &Assign(addr, value) => {
-                let v = (value & !mask_flags) | mask_values;
-                mem.insert(addr, v);
+            Assign(addr, value) => {
+                mem.insert(addr, (value & mask) | bits);
+            }
+        }
+    }
+
+    mem
+}
+
+// mask indicates X bits
+fn address_decoder(addr: u64, mask: u64) -> Vec<u64> {
+    if mask == 0 {
+        return vec![addr];
+    }
+
+    let last_bit = mask & (-(mask as i64) as u64);
+    let mut addrs = address_decoder(addr & !last_bit, mask & !last_bit);
+
+    for i in 0..addrs.len() {
+        addrs.push(addrs[i] | last_bit);
+    }
+
+    addrs
+}
+
+fn execute_v2(instrs: &[Instr]) -> HashMap<u64, u64> {
+    let mut mem = HashMap::new();
+    let (mut mask, mut bits) = (0, 0);
+
+    for &instr in instrs {
+        match instr {
+            Mask(m, v) => {
+                mask = m; // 0,1 -> 0, X -> 1
+                bits = v; // X,0 -> 0, 1 -> 1
+            }
+            Assign(addr, value) => {
+                for addr in address_decoder(addr | bits, mask) {
+                    mem.insert(addr, value);
+                }
             }
         }
     }
@@ -67,10 +102,14 @@ fn execute(instrs: &[Instr]) -> HashMap<u64, u64> {
 
 pub fn run() -> Result {
     let instrs = parse_input(&read_input("day14")?)?;
-    let mem = execute(&instrs);
 
-    let sum = sum(map(mem.values(), |&v| v));
-    println!("part A: {:?}", sum);
+    let mem = execute_v1(&instrs);
+    let total = sum(mem.values().copied());
+    println!("part A: {:?}", total);
+
+    let mem = execute_v2(&instrs);
+    let total = sum(mem.values().copied());
+    println!("part B: {:?}", total);
 
     Ok(())
 }
